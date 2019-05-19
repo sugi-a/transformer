3d65b4d9a2d566e07b955e9c361048d94715cc3e
sugi <sugi98765@gmail.com>
2019-05-19 09:04:17 +0000

    added random seed. improved fixed capacity batch logic

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,4 +1,4 @@
-import sys, os, codecs
+import sys, os, codecs, time
 from logging import getLogger
 logger = getLogger()
 
@@ -98,6 +98,140 @@ def make_dataset_source_target(
     if shuffle_size is not None:
         dataset = dataset.shuffle(shuffle_size)
     return dataset.map(lambda s,t: (__string2sequence(s, EOS_ID, tables[0]), __string2sequence(t, EOS_ID, tables[1])), ncpu)
+
+def make_source_target_zipped_list(
+                source_list,
+                target_list,
+                source_vocab_file_name,
+                target_vocab_file_name,
+                UNK_ID,
+                EOS_ID,
+                PAD_ID,
+                maxlen,
+                allow_skip=True):
+    '''
+    Args: Mostly the same as make_dataset_source_target.
+        maxlen: a sentence pair is ignored if one or both sentence in it has more tokens than `maxlen`
+        batch_capacity: batch's capacity (=shape[0]*shape[1]) doesn't exceed this value.
+    Returns:
+        List of nested structure:
+        [batches: (([batch_size: [length: int]], [batch_size: int]),
+                    ([batch_size: [length: int]], [batch_size: int]))]
+        Samples are sorted by the number of tokens in the source sentences and then batched from the beginning.
+        When batching, this method tries to put as many samples as possible into the batch while
+        keeping the batch's capacity (shape[0] * shape[1]) less than `batch_capacity`.
+        Note that in every training iteration, composition of each batch is invariant: shuffling in
+        every training iteration changes the order of batches but the contents in each batch remains the same.
+        
+    '''
+    _start_time = time.time()
+    logger.info('make_source_target_zipped_list')
+
+    # Make token->ID mapping
+    with codecs.open(source_vocab_file_name) as sv_f, codecs.open(target_vocab_file_name) as tv_f:
+        s_token2ID = {line.split()[0]: offset for offset, line in enumerate(sv_f)}
+        t_token2ID = {line.split()[0]: offset for offset, line in enumerate(tv_f)}
+
+    # Read lines from the source/target file and zip
+    # [dataset size: ([source length: str], [target length: str])]
+    zipped_lines = [(sl.strip().split(' '), tl.strip().split(' ')) for sl,tl in zip(source_list, target_list)]
+
+    # Make batches
+    logger.info('#pairs in the original dataset:{}'.format(len(zipped_lines)))
+
+    # Convert to ID, add EOS and check length
+    new_zipped_lines = []
+    n_ignored_pairs = 0
+    for s_seq, t_seq in zipped_lines:
+        # Convert tokens to IDs
+        s_seq = [s_token2ID.get(token, UNK_ID) for token in s_seq if len(token) > 0]
+        t_seq = [t_token2ID.get(token, UNK_ID) for token in t_seq if len(token) > 0]
+
+        # Add EOS
+        if EOS_ID is not None:
+            s_seq = s_seq + [EOS_ID]
+            t_seq = t_seq + [EOS_ID]
+
+        # get sequence length
+        s_len, t_len = len(s_seq), len(t_seq)
+
+        # Skip too long sequences
+        if s_len > maxlen or t_len > maxlen:
+            assert allow_skip
+            n_ignored_pairs += 1
+            continue
+        else:
+            new_zipped_lines.append((s_seq, t_seq))
+
+    logger.info('make_source_target_zipped_list')
+    logger.info('''make_source_target_zipped_list done.
+Number of ignored pairs:{}. Time: {}sec'''.format(n_ignored_pairs, time.time() - _start_time))
+
+    return new_zipped_lines
+
+def make_batches_from_zipped_list(
+                zipped_lines,
+                PAD_ID,
+                batch_capacity,
+                sort=False,
+                shuffle=False,
+                allow_skip=False):
+
+    _start_time = time.time()
+    logger.info('Making batches. #pairs in the original dataset:{}'.format(len(zipped_lines)))
+
+    assert not (sort and shuffle)
+    if sort:
+        zipped_lines.sort(lambda x: len(x[0]))
+    if shuffle:
+        zipped_lines = np.random.permutation(zipped_lines)
+
+    batches = []
+    s_batch, t_batch, s_lens, t_lens = None, None, None, None
+    batch_size = 0 # batch_shape[0]
+    batch_length = 1e9 # batch_shzpe[1]
+    n_ignored_pairs = 0
+    for s_seq, t_seq in zipped_lines:
+        # get sequence length
+        s_len, t_len = len(s_seq), len(t_seq)
+
+        # Skip too long sequences
+        if s_len > batch_capacity or t_len > batch_capacity:
+            assert allow_skip
+            n_ignored_pairs += 1
+            continue
+
+        # Update length and size
+        batch_length = max(batch_length, s_len, t_len)
+        batch_size += 1
+
+        # Make a new minibatch if overflow
+        if batch_size * batch_length > batch_capacity:
+            s_batch, t_batch, s_lens, t_lens = [], [], [], []
+            batches.append((s_batch, s_lens, t_batch, t_lens))
+            batch_size = 1
+            batch_length = max(s_len, t_len)
+        s_batch.append(s_seq)
+        t_batch.append(t_seq)
+        s_lens.append(s_len)
+        t_lens.append(t_len)
+    logger.info('''Making batches done. Number of ignored pairs:{}
+Number of batches:{}, time:{}sec'''.format(
+    n_ignored_pairs, len(batches), time.time()-_start_time))
+
+    # Pad batches. structure: ((seq, len), (seq, len))
+    _start_time = time.time()
+    logger.info('Padding batches.')
+    padded_batches = []
+    for s_batch, s_lens, t_batch, t_lens in batches:
+        s_batch_length = max(s_lens)
+        t_batch_length = max(t_lens)
+        padded_s_batch = [seq + [PAD_ID] * (s_batch_length - l) for seq,l in zip(s_batch, s_lens)]
+        padded_t_batch = [seq + [PAD_ID] * (t_batch_length - l) for seq,l in zip(t_batch, t_lens)]
+        padded_batches.append(((padded_s_batch, s_lens), (padded_t_batch, t_lens)))
+    logger.info('Padding batches done. time: {}sec'.format(time.time() - _start_time))
+
+    return padded_batches
     
 def make_batches_source_target_const_capacity_batch_from_list(
                 source_list,
@@ -109,8 +243,8 @@ def make_batches_source_target_const_capacity_batch_from_list(
                 PAD_ID,
                 maxlen,
                 batch_capacity,
-                ncpu=8,
-                sort=True,
+                sort=False,
+                shuffle=False,
                 allow_skip=True):
     '''
     Args: Mostly the same as make_dataset_source_target.
@@ -131,72 +265,13 @@ def make_batches_source_target_const_capacity_batch_from_list(
 
     assert maxlen <= batch_capacity
 
-    # Make token->ID mapping
-    with codecs.open(source_vocab_file_name) as sv_f, codecs.open(target_vocab_file_name) as tv_f:
-        s_token2ID = {line.split()[0]: offset for offset, line in enumerate(sv_f)}
-        t_token2ID = {line.split()[0]: offset for offset, line in enumerate(tv_f)}
-
-    # Read lines from the source/target file and zip
-    # [dataset size: ([source length: str], [target length: str])]
-    zipped_lines = [(sl.strip().split(' '), tl.strip().split(' ')) for sl,tl in zip(source_list, target_list)]
-
-    # Sort by the number of tokens in the source sentence
-    if sort:
-        zipped_lines.sort(key=lambda x:len(x[0]))
+    zipped_list = make_source_target_zipped_list(source_list, target_list,
+        source_vocab_file_name, target_vocab_file_name,
+        UNK_ID, EOS_ID, PAD_ID, maxlen, allow_skip)
 
     # Make batches
-    logger.info('Making batches. #pairs in the original dataset:{}'.format(len(zipped_lines)))
-    batches = []
-    s_batch, t_batch, s_lens, t_lens = None, None, None, None
-    batch_size = 0 # batch_shape[0]
-    batch_length = 1e9 # batch_shzpe[1]
-    n_ignored_pairs = 0
-    for s_seq, t_seq in zipped_lines:
-        # Convert tokens to IDs
-        s_seq = [s_token2ID.get(token, UNK_ID) for token in s_seq if len(token) > 0]
-        t_seq = [t_token2ID.get(token, UNK_ID) for token in t_seq if len(token) > 0]
-
-        # Add EOS
-        if EOS_ID is not None:
-            s_seq = s_seq + [EOS_ID]
-            t_seq = t_seq + [EOS_ID]
-
-        # get sequence length
-        s_len, t_len = len(s_seq), len(t_seq)
-
-        # Skip too long sequences
-        if s_len > maxlen or t_len > maxlen or s_len > batch_capacity or t_len > batch_capacity:
-            assert allow_skip
-            n_ignored_pairs += 1
-            continue
-
-        # Update length and size
-        batch_length = max(batch_length, s_len, t_len)
-        batch_size += 1
-
-        # Make a new minibatch if overflow
-        if (batch_size + 1) * batch_length > batch_capacity:
-            s_batch, t_batch, s_lens, t_lens = [], [], [], []
-            batches.append((s_batch, s_lens, t_batch, t_lens))
-            batch_size = 1
-            batch_length = max(s_len, t_len)
-        s_batch.append(s_seq)
-        t_batch.append(t_seq)
-        s_lens.append(s_len)
-        t_lens.append(t_len)
-    logger.info('''Making batches done. Number of ignored pairs:{}
-                Number of batches:{}'''.format(n_ignored_pairs, len(batches)))
-
-    # Pad batches. structure: ((seq, len), (seq, len))
-    logger.info('Padding batches.')
-    padded_batches = []
-    for s_batch, s_lens, t_batch, t_lens in batches:
-        s_batch_length = max(s_lens)
-        t_batch_length = max(t_lens)
-        padded_s_batch = [seq + [PAD_ID] * (s_batch_length - l) for seq,l in zip(s_batch, s_lens)]
-        padded_t_batch = [seq + [PAD_ID] * (t_batch_length - l) for seq,l in zip(t_batch, t_lens)]
-        padded_batches.append(((padded_s_batch, s_lens), (padded_t_batch, t_lens)))
-    logger.info('Padding batches done.')
+    padded_batches = make_batches_from_zipped_list(zipped_list,
+        PAD_ID, batch_capacity, sort, shuffle, allow_skip)
 
     return padded_batches
 
@@ -210,8 +285,8 @@ def make_dataset_source_target_const_capacity_batch_from_list(
                 PAD_ID,
                 maxlen,
                 batch_capacity,
-                ncpu=8,
-                sort=True,
+                sort=False,
+                shuffle=False,
                 allow_skip=True):
     '''
     Args: Mostly the same as make_dataset_source_target.
@@ -226,12 +301,16 @@ def make_dataset_source_target_const_capacity_batch_from_list(
         
     '''
     logger.info('make_dataset_source_target_const_capacity_batch_from_list')
+    assert maxlen <= batch_capacity
 
+    zipped_list = make_source_target_zipped_list(source_list, target_list,
+        source_vocab_file_name, target_vocab_file_name,
+        UNK_ID, EOS_ID, PAD_ID, maxlen, allow_skip)
+
+    # Make batches
     def gen():
-        return make_batches_source_target_const_capacity_batch_from_list(
-            source_list, target_list, source_vocab_file_name,
-            target_vocab_file_name, UNK_ID, EOS_ID, PAD_ID, maxlen,
-            batch_capacity, ncpu, sort, allow_skip)
+        return make_batches_from_zipped_list(zipped_list,
+            PAD_ID, batch_capacity, sort, shuffle, allow_skip)
 
     # Make dataset
     dataset = tf.data.Dataset.from_generator(
@@ -245,16 +324,7 @@ def make_dataset_source_target_const_capacity_batch_from_list(
 def make_dataset_source_target_const_capacity_batch(
                 source_file_name,
                 target_file_name,
-                source_vocab_file_name,
-                target_vocab_file_name,
-                UNK_ID,
-                EOS_ID,
-                PAD_ID,
-                maxlen,
-                batch_capacity,
-                ncpu=8,
-                sort=True,
-                allow_skip=True):
+                *args, **kwargs):
     '''
     Args: Mostly the same as make_dataset_source_target.
         maxlen: a sentence pair is ignored if one or both sentence in it has more tokens than `maxlen`
@@ -282,9 +352,10 @@ def make_dataset_source_target_const_capacity_batch(
             source_lines = s_f.readlines()
             target_lines = t_f.readlines()
 
-    return make_dataset_source_target_const_capacity_batch_from_list(souce_lines,
-        target_lines, source_vocab_file_name, target_vocab_file_name, UNK_ID,
-        EOS_ID, PAD_ID, maxlen, batch_capacity, ncpu, sort, allow_skip)
+    return make_dataset_source_target_const_capacity_batch_from_list(
+        source_lines,
+        target_lines,
+        *args, **kwargs)
 
 def make_dataset_single(file_name, vocab_file_name, UNK_ID, EOS_ID, ncpu=8):
     table = tf.contrib.lookup.index_table_from_file(
