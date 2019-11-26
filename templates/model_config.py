29085d38b299856dcc9b13db0b570eaaa7a5559e
sugi <sugi98765@gmail.com>
2019-11-26 08:02:53 +0000

    Drastically modified the framework design. Clarified the principle: multiple models in one run - You can load multiple transformer models in a single process - no longer use import to load model_config.py

--- templates/model_config.py
+++ templates/model_config.py
@@ -1,16 +1,30 @@
 import os, json
 """
-What this module must have:
-    params: configuration of the Transformer and training. (dict)
-What this module can have:
-    IDs2tokens: method to convert IDs [nlines, maxlen] into tokens (function)
-    validation_metric: validation function
+Configuration for a model.
+
+This file is loaded by python3
+`exec(this_file_content, None, {'model_dir': dirname_of_this_file})`
+
+This file must contain:
+    params : a dict storing configuration of Transformer and training.
+This file can contain:
+    IDs2text: method to convert token IDs in the target language into sentences.
+        Args:
+            IDs: list of list of int
+        Returns:
+            List of str. Just replace indivisual IDs by tokens. (Do not spm_decode)
+    validation_metric: validation method
 """
 
-with open(os.path.dirname(__file__) + '/' + 'model_config.json') as f:
+
+# -------- `params` --------
+# By default, automatically built from `model_config.json`
+
+with open(model_dir + '/' + 'model_config.json') as f:
     params = json.load(f)
 
-if 'basedir' in params: # add prefix to the dataset paths
+# Add prefix to the dataset paths. Absolute prefix is recommended.
+if 'basedir' in params: 
     _p = params["basedir"] + '/'
     for k in ["source_train", "target_train"]:
         for i in range(len(params["train"]["data"][k])):
@@ -20,34 +34,31 @@ if 'basedir' in params: # add prefix to the dataset paths
     params["vocab"]["source_dict"] = _p + params["vocab"]["source_dict"]
     params["vocab"]["target_dict"] = _p + params["vocab"]["target_dict"]
 
+
+# -------- methods --------
 # Following two functions can be customly defined
 """
-def IDs2tokens(IDs, lang):
-    '''IDs: list of list of int'''
-    pass
-
-def validation_metric(global_step, inference):
-    return <score>
+IDs2text(IDs)
+validation_metric(global_step, inference)
 """
 
-# Some examples:
+# Here is an example of the two methods
 
 """
 import sentencepiece as spm
 sp = spm.SentencePieceProcessor()
-sp.Load(<path to spm file>)
+sp.Load(params["vocab"]["source_dict"][:-len("vocab")] + "model")
 
-def IDs2tokens(IDs):
-    '''IDs: list of list of int'''
+def IDs2text(IDs):
+    '''
+    IDs: list of list of int
+    Returns: list of sentence'''
     return [' '.join(sp.id_to_piece(id) for id in sent if not sp.is_control(id)) for sent in IDs]
 
-# def IDs2tokens(IDs):
-#     '''IDs: list of list of int'''
-#     return [sp.DecodeIds(sent) for sent in IDs]
 
-with open(<dev_src>) as f:
+with open(params["train"]["data"]["source_dev"]) as f:
     __src_lines = [line.strip() for line in f]
-with open(<dev_trg>) as f:
+with open(params["train"]["data"]["target_dev"]) as f:
     __trg_lines = [line.strip() for line in f]
 
     # decode, split, wrap
