e645648073c19dd327009b35f11c60b56c89e9b4
sugi <sugi98765@gmail.com>
2019-12-27 08:48:34 +0000

    added calculation of cumulative score along time axis

--- extras/decode_analysis.py
+++ extras/decode_analysis.py
@@ -2,8 +2,9 @@ import sys, os, re, json, argparse
 import tensorflow as tf
 import numpy as np
 
-from ..components.inference import Inference
 from tensorflow.contrib.framework import nest
+from ..components.inference import Inference
+from ..components.decoding import length_penalty
 
 class DecodeAnalysis(Inference):
     def __init__(self, *args, **kwargs):
@@ -11,6 +12,10 @@ class DecodeAnalysis(Inference):
         
         self.ph_alt_topk = tf.placeholder(tf.int32, [])
         self.op_topk_alts = self.make_op(self.fn_topk_alternatives)
+
+        self.op_cum_logp = self.make_op(self.fn_cumulative_log_prob, 'logp')
+        self.op_cum_score = self.make_op(self.fn_cumulative_log_prob, 'score')
+        self.op_cum_ppl = self.make_op(self.fn_cumulative_log_prob, 'ppl')
         self.make_session()
 
     def fn_topk_alternatives(self, inputs):
@@ -25,6 +30,29 @@ class DecodeAnalysis(Inference):
 
         return [inds, top_probs]
 
+
+    def fn_cumulative_log_prob(self, inputs, mode):
+        (x, x_len), (y, y_len) = inputs
+        logits = self.model.get_logits(x, y, x_len, y_len, False)
+        is_target = tf.sequence_mask(y_len, tf.shape(y)[1], dtype=tf.float32)
+
+        logp = tf.math.log_softmax(logits, axis=-1)
+
+        # [batch, len, vocab] -> [batch, len]
+        logp = tf.batch_gather(logp, y[:,:, None])[:, :, 0]
+
+        # cumulative sum of log probabilities
+        logp = tf.math.cumsum(logp, axis=1)
+
+        assert mode in ['logp', 'score', 'ppl']
+        if mode == 'logp':
+            pass
+        elif mode == 'score':
+            logp = logp / length_penalty(tf.range(tf.shape(y)[1]) + 1, self.params['test']['length_penalty_a'])
+        elif mode == 'ppl':
+            logp = tf.math.exp(- logp / tf.cast(tf.range(tf.shape(y)[1]) + 1, tf.float32))
+
+        return logp,
 
 
     def get_topk_alts(self, sources, targets, k=None):
@@ -50,6 +78,23 @@ class DecodeAnalysis(Inference):
 
         return output
 
+
+    def get_cumulative_log_prob(self, sources, targets, mode):
+        batches = self.make_batches(sources, targets)
+
+        if mode == 'logp':
+            logp, = self.execute_op(self.op_cum_logp, batches) 
+        elif mode == 'score':
+            logp, = self.execute_op(self.op_cum_score, batches) 
+        elif mode == 'ppl':
+            logp, = self.execute_op(self.op_cum_ppl, batches) 
+        else:
+            assert False
+
+        ret = [l[:len(t.split()) + 1] for l, t in zip(logp, targets)]
+
+        return ret
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', '--dir', '-d', type=str, required=True)
@@ -60,6 +105,12 @@ def main():
     parser_alts.add_argument('source_file', type=str)
     parser_alts.add_argument('target_file', type=str)
     parser_alts.add_argument('-k', type=int, default=None)
+
+    parser_cumlogp = sub_parsers.add_parser('cumlogp')
+    parser_cumlogp.add_argument('source_file', type=str)
+    parser_cumlogp.add_argument('target_file', type=str)
+    parser_cumlogp.add_argument('--type', '-t', choices=['logp', 'score', 'ppl'], default='logp')
+    parser_cumlogp.add_argument('--print-toks', action='store_true')
 
     args = parser.parse_args()
 
@@ -72,7 +123,16 @@ def main():
             targets = f.readlines()
         
         print(dec_analysis.get_topk_alts(sources, targets, args.k))
-        
+    elif args.mode == 'cumlogp':
+        with open(args.source_file, 'r') as f:
+            sources = f.readlines()
+        with open(args.target_file, 'r') as f:
+            targets = f.readlines()
+
+        logp = dec_analysis.get_cumulative_log_prob(sources, targets, args.type)
+
+        for l in logp:
+            print('\t'.join(map(str, l)))
 
 
 if __name__ == '__main__':
