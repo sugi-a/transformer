8260747387827e1910da4f5b0550a57bc5985583
sugi <sugi98765@gmail.com>
2020-10-18 22:32:07 +0900

    updated relative_position for tf2.0

--- vanilla/relative_position.py
+++ vanilla/relative_position.py
@@ -1,101 +1,99 @@
 import tensorflow as tf
+from tensorflow import keras
 
 
-class RelativePositionMultiheadSelfAttention(tf.layers.Layer):
-    def __init__(self, hidden_size, n_heads, dropout_rate, max_relative_dist, unique_per_head, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        assert hidden_size % n_heads == 0
-
-        self.hidden_size = hidden_size
+class RelativePositionMultiheadSelfAttention(keras.layers.Layer):
+    def __init__(
+            self, d_model, n_heads, dropout_rate,
+            max_relative_dist, unique_per_head, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.d_model = d_model
         self.n_heads = n_heads
-        self.head_size = self.hidden_size // self.n_heads
-        self.dropout_rate = dropout_rate
-        self.max_relative_dist = max_relative_dist
+        self.max_dist = max_relative_dist
         self.unique_per_head = unique_per_head
 
-    
-    def build(self, input_shape):
-        self.q_layer = tf.layers.Dense(self.hidden_size, use_bias=False, name='q_layer')
-        self.k_layer = tf.layers.Dense(self.hidden_size, use_bias=False, name='k_layer')
-        self.v_layer = tf.layers.Dense(self.hidden_size, use_bias=False, name='v_layer')
-        self.att_out_layer = tf.layers.Dense(self.hidden_size, use_bias=False, name='attention_output')
+        self.q = keras.layers.Dense(self.d_model, use_bias=False, name='q')
+        self.k = keras.layers.Dense(self.d_model, use_bias=False, name='k')
+        self.v = keras.layers.Dense(self.d_model, use_bias=False, name='v')
+        self.dropout = keras.layers.Dropout(dropout_rate)
+        self.out = tf.layers.Dense(self.d_model, use_bias=False, name='out')
 
-        
-        if self.unique_per_head:
+        if unique_per_head:
             # [vocab, nheads, head_size]
-            self.pos_emb = tf.get_variable('embedding', [self.max_relative_dist * 2 + 1, self.n_heads, self.head_size], tf.float32)
+            self.pos_emb = self.add_weight(
+                shape=[max_relative_dist * 2 + 1, n_heads, d_model // n_heads],
+                dtype=tf.float32)
         else:
             # [vocab, head_size]
-            self.pos_emb = tf.get_variable('embedding', [self.max_relative_dist * 2 + 1, self.head_size], tf.float32)
+            self.pos_emb = self.add_weight(
+                shape=[max_relative_dist * 2 + 1, d_model // n_heads],
+                dtype=tf.float32)
+        
 
-        super().build(input_shape)
+    def call(self, query, bias, training, cache=None):
+        head_size = self.d_model // self.n_heads
 
-
-    def call(self, query, bias, training=False, cache=None):
-        q = self.q_layer(query)
-
-        q = self.q_layer(query) # [batch, length, hidden]
-        k = self.k_layer(query) # same as above
-        v = self.v_layer(query) # same as above
-
+        # [B, L, E]
+        q = self.q(query)
+        k = self.k(query)
+        v = self.v(query)
         
         if cache is not None:
-            with tf.name_scope('layer_cache_extension'):
-                k = tf.concat([cache['k'], k], axis=1)
-                v = tf.concat([cache['v'], v], axis=1)
-                cache['k'] = k
-                cache['v'] = v
+            k = tf.concat([cache['k'], k], axis=1)
+            v = tf.concat([cache['v'], v], axis=1)
+            cache['k'] = k
+            cache['v'] = v
 
-        # Reshape to [batch, nheads, length_q, head_size]
+        # [B, nheads, L_q, H]
         q = tf.stack(tf.split(q, self.n_heads, axis=-1), axis=1)
-        # Reshape to [batch, nheads, length_k, head_size]
+        # [B, nheads, L_k, H]
         k = tf.stack(tf.split(k, self.n_heads, axis=-1), axis=1)
         v = tf.stack(tf.split(v, self.n_heads, axis=-1), axis=1)
 
         # Normal q-k multiplication term
-        weight = tf.matmul(q, k, transpose_b=True) # [batch, nheads, length_q, length_k]
+        # [B, nheads, L_q, L_k]
+        weight = tf.matmul(q, k, transpose_b=True)
 
-        with tf.name_scope('relative_position_bias'):
-            # Make relative position matrix. [q_len, k_len]
-            q_len, k_len = tf.shape(q)[2], tf.shape(k)[2]
-            k_indices = tf.range(k_len)
-            q_indices = k_indices[-q_len:]
-            rel_pos = k_indices[None, :] - q_indices[:, None]
+        # [q_len, k_len] Relative position matrix
+        L_q, L_k = tf.shape(q)[2], tf.shape(k)[2]
+        k_indices = tf.range(L_k)
+        q_indices = k_indices[-L_q:]
+        rel_pos = k_indices[None, :] - q_indices[:, None]
 
-            # Clipping
-            rel_pos = tf.clip_by_value(rel_pos, -self.max_relative_dist, self.max_relative_dist)
+        # Clipping
+        rel_pos = tf.clip_by_value(rel_pos, -self.max_dist, self.max_dist)
 
-            # Shift to start from 0
-            rel_pos += self.max_relative_dist
+        # Shift to start from 0
+        rel_pos += self.max_dist
 
-            # Make embedding matrix. If unique_per_head: [q_len, k_len, head_size], else: [q_len, k_len, nheads, head_size]
-            embeddings = tf.gather(self.pos_emb, rel_pos)
+        # Embedding matrix.
+        # [L_q, L_k, head_size] If unique_per_head
+        # [L_q, L_k, nheads, head_size] Otherwise
+        embeddings = tf.gather(self.pos_emb, rel_pos)
 
-            # Make bias. [batch, nheads, q_len, k_len]
-            if self.unique_per_head:
-                # q: [batch, nheads, q_len, head], emb: [q_len, k_len, nheads, head]
-                _rel_pos_bias = tf.einsum('bnqh,qknh->bnqk', q, embeddings)
-            else:
-                # q: [batch, nheads, q_len, head], emb: [q_len, k_len, head]
-                _rel_pos_bias = tf.einsum('bnqh,qkh->bnqk', q, embeddings)
+        # Make bias. [batch, nheads, L_q, L_k]
+        if self.unique_per_head:
+            # q: [batch, nheads, L_q, head], emb: [L_q, L_k, nheads, head]
+            rel_pos_bias = tf.einsum('bnqh,qknh->bnqk', q, embeddings)
+        else:
+            # q: [batch, nheads, L_q, head], emb: [L_q, L_k, head]
+            rel_pos_bias = tf.einsum('bnqh,qkh->bnqk', q, embeddings)
 
-            # Apply relative postion bias to `weight`
-            weight += _rel_pos_bias
+        # Apply relative postion bias
+        weight += rel_pos_bias
 
-        
-        weight = weight / (self.head_size ** 0.5)
-
-        with tf.name_scope('add_bias'):
-            weight = weight + bias
+        weight = weight / (head_size ** 0.5)
+        weight = weight + bias
 
         weight = tf.nn.softmax(weight, name='attention_weight')
+        weight = self.dropout(weight, training=training)
 
-        weight = tf.layers.dropout(weight, self.dropout_rate, training=training)
+        # [batch, nheads, length_q, head_size]
+        outputs = tf.matmul(weight, v)
 
-        outputs = tf.matmul(weight, v) # [batch, nheads, length_q, head_size]
-
-        outputs = tf.concat(tf.unstack(outputs, axis=1), axis=2) # [batch, length_q, emb_size]
+        # [batch, length_q, emb_size]
+        outputs = tf.concat(tf.unstack(outputs, axis=1), axis=2)
 
         outputs = self.att_out_layer(outputs)
 
